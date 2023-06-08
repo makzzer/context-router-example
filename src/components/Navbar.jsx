8409da6565958f494e51c5785200d7146272a4bd
Maximiliano <makzofx@gmail.com>
2023-06-08 14:29:03 -0300

    arreglado el useContext en NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { useUserContext } from "../context/UserContext";
 
@@ -8,17 +7,21 @@ import { useUserContext } from "../context/UserContext";
 const Navbar = () => {
 
  
-  const {user,setUser} = useUserContext
+  const {user,setUser} = useUserContext()
   //el hook useContext --> me permite acceder al estado del UserContext que definí antes
-  console.log(useUserContext)
+  
+  console.log(user)
 
   return (
     <nav className="navbar navbar-dark bg-dark p-4 d-flex justify-content-center gap-2">
     <NavLink to="/"> Home </NavLink>
     {
+      user 
+      ? <NavLink to="/dashboard">Dashboard</NavLink>
+      :<NavLink to="/dashboard">No podes pasar</NavLink>
 
     }
-    <NavLink to="/dashboard">Dashboard</NavLink>
+    
   </nav>
   );
 };
